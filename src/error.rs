429214401aad76727959dbd0e3d53476480f91f1
Mikael Pettersson <mikael.pettersson201@gmail.com>
2026-09-20 19:38:06 +0200

    feat: add runtime errors

---
+++ src/error.rs
@@ -0,0 +1,13 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum KnutError {
+    #[error("system one failed: {0}")]
+    SystemOne(String),
+
+    #[error("tool not found: {0}")]
+    ToolNotFound(String),
+
+    #[error("tool failed: {0}")]
+    Tool(String),
+}
